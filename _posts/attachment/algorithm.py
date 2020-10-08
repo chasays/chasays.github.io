3f2b82396901c32cf02b3b6eecd658f2f6712e6d
叉叉敌 <xxd0225@gmail.com>
2020-10-08 16:43:15 +0800

    add python list implementation

--- _posts/attachment/algorithm.py
+++ _posts/attachment/algorithm.py
@@ -22,11 +22,36 @@ def findFirstAlpha(s):
     print(ret)
     return ret
         
+def isCardStraight(cards):
+    tmpCards = [0] * 10 ## todo JQK
+    if not isinstance(cards, list) or len(cards) != 5:
+        raise ValueError("please correct cards value")
+    for card in cards:
+        tmpCards[card-1] = card
+    print(tmpCards)
+    for i, v in enumerate(tmpCards):
+        print(i, v)
+        if v != 0 and i < 6 and 0 not in tmpCards[i:i+5]: # out of list
+                return True
+    return False
+
+def isStraight(cards):
+    if 1 in cards and 13 in cards:
+        cards[cards.index(1)]=14
+    if 3 == sum(set([abs(x-sum(cards)/5) for x in cards])):
+        return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
     tmplist = [5, 4, 3, 2, 1]
+    tmplist = [11, 10, 12, 13, 1]
     # print(bubble((tmplist)))
-    s = '1234123'
-    findFirstAlpha(s)
+    # s = '1234123'
+    # findFirstAlpha(s)
+    # print(isCardStraight(tmplist))
+    print(isStraight(tmplist))
         
 
 
