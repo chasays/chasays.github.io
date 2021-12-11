e606ea421b5fb1b4024ccf1fa4136140dce721a0
chasays <xxd0225@gmail.com>
2021-12-11 18:33:29 +0800

    15-黑胶

--- _posts/attachment/basic.py
+++ _posts/attachment/basic.py
@@ -1,16 +1,6 @@
-# 注释
-# 缩进
+# if 语句
 
-# 这个是注释 1
-"""这个是注释2 双引号"""
-'''这个是注释3 单引号'''
-
-def sum(a, b):
-   # 求 a 和 b 的和
-    print(a)
-    return a + b
+num = 3
+if num > 0:
+    print(num, "是正数。")
     
-
-# 调用sum 函数
-sum_1 = sum(1, 2)
-print(f"a和b的和是：{sum_1}")
